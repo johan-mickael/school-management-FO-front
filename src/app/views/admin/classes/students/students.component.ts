8e931f0bbc6a008b2459be98dce5facbd2498f05
johan <rakotoniainajohanmickael@gmail.com>
2022-01-13 16:48:42 +0100

    Student table to list

--- src/app/views/admin/classes/students/students.component.ts
+++ src/app/views/admin/classes/students/students.component.ts
@@ -1,9 +1,9 @@
-import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
-import { ResourceService } from '../../../../services/resource.service';
-import { NgxSpinnerService } from 'ngx-spinner';
-import { ErrorService } from '../../../../services/error.service';
-import { SchoolYear, Student, Subclass } from '../../../../services/interfaces';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core'
+import { ResourceService } from '../../../../services/resource.service'
+import { NgxSpinnerService } from 'ngx-spinner'
+import { ErrorService } from '../../../../services/error.service'
+import { SchoolYear, Student, Subclass } from '../../../../services/interfaces'
+import { ActivatedRoute, Router } from '@angular/router'
 
 @Component({
   selector: 'app-students',
@@ -20,13 +20,14 @@ export class StudentsComponent implements OnInit, OnChanges {
     private router: Router
   ) { }
 
-  @Input() subclass: Subclass;
-  schoolYearId: number;
-  selectedSchoolyear: SchoolYear;
-  schoolYears: SchoolYear[];
-  students: any;
-  dataLoaded: Promise<boolean>;
+  @Input() subclass: Subclass
+  schoolYearId: number
+  selectedSchoolyear: SchoolYear
+  schoolYears: SchoolYear[]
+  students: any
+  dataLoaded: Promise<boolean>
   loadingCount: number = 0
+  selectedStudent: any
 
   dtOptions = {
     pagingType: 'full_numbers',
@@ -35,38 +36,25 @@ export class StudentsComponent implements OnInit, OnChanges {
   }
 
   async ngOnInit() {
-    await this.fetchApiData();
+    await this.fetchApiData()
   }
-
   async ngOnChanges(changes: SimpleChanges) {
-    this.loadingCount ++
-    if (this.loadingCount == 1) return;
-    this.getStudents()
+    await this.spinnerService.show('student-spinner')
+    this.loadingCount++
+    if (this.loadingCount == 1) return
+    await this.getStudents()
+    this.spinnerService.hide('student-spinner')
   }
-
   async onSchoolYearChange() {
+    await this.spinnerService.show('student-spinner')
     this.selectedSchoolyear = this.schoolYears.filter(schoolYear => {
       return schoolYear.id == this.schoolYearId
     })[0]
-    this.getStudents()
+    await this.getStudents()
+    this.spinnerService.hide('student-spinner')
   }
-
-  async getStudents() {
-    await this.spinnerService.show('student-spinner');
-    try {
-      const data = await Promise.resolve(
-        this.resourceService.getPromise(this.resourceService.findAll('subclasses/students/' + this.subclass.id + '/' + this.schoolYearId))
-      )
-      this.students = data as Student[]
-      this.dataLoaded = Promise.resolve(true)
-      await this.spinnerService.hide('student-spinner');
-    } catch (error) {
-      this.errorService.handleError(error, 'student-spinner');
-    }
-  }
-
   async fetchApiData() {
-    await this.spinnerService.show('student-spinner');
+    await this.spinnerService.show('student-spinner')
     try {
       this.schoolYears = await Promise.resolve(
         this.resourceService.getPromise(this.resourceService.findAll('schoolyears'))
@@ -75,14 +63,21 @@ export class StudentsComponent implements OnInit, OnChanges {
       this.selectedSchoolyear = this.schoolYears.filter(schoolYear => {
         return schoolYear.id == this.schoolYearId
       })[0]
+      await this.getStudents()
+      this.spinnerService.hide('student-spinner')
+    } catch (error) {
+      this.errorService.handleError(error, 'student-spinner')
+    }
+  }
+  async getStudents() {
+    try {
       this.students = await Promise.resolve(
         this.resourceService.getPromise(this.resourceService.findAll('subclasses/students/' + this.subclass.id + '/' + this.schoolYearId))
       )
+      this.selectedStudent = this.students[0]
       this.dataLoaded = Promise.resolve(true)
-      this.spinnerService.hide('student-spinner');
     } catch (error) {
-      this.errorService.handleError(error, 'student-spinner');
+      this.errorService.handleError(error, 'student-spinner')
     }
   }
-
 }
